47cc49be4479eeb64b9e7d7c2a532e8d201561d7
Marwan <marwan.hatem0605@gmail.com>
2026-09-03 14:44:03 +0300

    defined database funcs

--- SDB.h
+++ SDB.h
@@ -10,3 +10,12 @@ uint32 Course2_grade;
 uint32 Course3_ID;
 uint32 Course3_grade;
 } student;
+
+
+uint8 SDB_GetUsedSize();
+bool SDB_IsFull();
+bool SDB_AddEntry();
+bool SDB_ReadEntry(uint32 id);
+bool SDB_IsIdExist(uint32 id);
+void SDB_DeletEntry(uint32 id);
+void SDB_GetList(uint8 *count, uint32 *list);
