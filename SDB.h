6eb57c7d836798f1a5c9573d63fde77a54e48fe0
Marwan <marwan.hatem0605@gmail.com>
2026-09-03 03:19:45 +0300

    Std_types and Student Struct

---
+++ SDB.h
@@ -0,0 +1,12 @@
+#include"STD.h"
+
+typedef struct SimpleDb{
+uint32 Student_ID;
+uint32 Student_year;
+uint32 Course1_ID;
+uint32 Course1_grade;
+uint32 Course2_ID;
+uint32 Course2_grade;
+uint32 Course3_ID;
+uint32 Course3_grade;
+} student;
